12a8dabdb42df0053ae13474274433cff57294ef
mollyozzie <molly.osburg@gmail.com>
2021-08-18 13:07:55 -0500

    home controller and index initialized

---
+++ src/main/java/org/launchcode/birdistheword/controllers/BirdController.java
@@ -0,0 +1,10 @@
+package org.launchcode.birdistheword.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("birds")
+public class BirdController {
+
+}
