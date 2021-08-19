bb36b9b6ad096782e4793420eca5ce774f5275eb
mollyozzie <molly.osburg@gmail.com>
2021-08-19 13:31:48 -0500

    added bird log form and handler

--- src/main/java/org/launchcode/birdistheword/controllers/BirdController.java
+++ src/main/java/org/launchcode/birdistheword/controllers/BirdController.java
@@ -3,24 +3,35 @@ package org.launchcode.birdistheword.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Controller
 @RequestMapping("birds")
 public class BirdController {
 
+    private static List<String> birds = new ArrayList<>();
+
     @GetMapping
     public String displayAllBirds(Model model) {
-        List<String> birds = new ArrayList<>();
-        birds.add("Cardinal");
-        birds.add("Bluejay");
-        birds.add("Cooper's Hawk");
-        birds.add("Grackle");
         model.addAttribute("birds", birds);
         return "birds/index";
+    }
+
+    @GetMapping("log")
+    public String renderAddBirdForm() {
+        return "birds/log";
+    }
+
+    @PostMapping("log")
+    public String processAddBirdForm(@RequestParam String birdSpecies) {
+        birds.add(birdSpecies);
+        return "redirect:";
     }
 
 }
